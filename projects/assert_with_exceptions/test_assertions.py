ea57b59392b43587c0db39e655138e7fa0d15d95
elastic <elastic7327@gmail.com>
2017-10-12 14:44:26 +0900

    dd

---
+++ projects/assert_with_exceptions/test_assertions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+이것이... module doc string...
+"""
+
+import unittest
+
+class TestForAsserTions(unittest.TestCase):
+    """Test case docstring."""
+    def test_name(self):
+        """
+        test_name
+        """
+        try:
+            assert 1 is not 1, "The Balance is negative numeric"
+
+        except AssertionError as e:
+            print(e.message)
+            print(e)
+
+    def test_is_negative_number(self):
+        """
+        test_name
+        """
+        try:
+            the_number = -100
+            assert the_number >= 0, "The_number suppose to be greter than 0!!"
+
+        except AssertionError as e:
+            print(e.message)
+            print(e)
+            # raise 를 하게되면 무조건 Fail을 시킵니다...1
+
+        except Exception as e:
+            print ("Exception Part")
+            raise
+
+        finally:
+            # 무조건 finally 로 가기떄문에, 조심
+            print ("Finally Part")
+            # raise
